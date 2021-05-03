852bff2f1a96c542b4f94570ae4a8e22b0aee9b7
schen6 <sailchen@gmail.com>
2021-05-03 09:21:12 +0800

    token

--- weibo/settings.py
+++ weibo/settings.py
@@ -43,7 +43,7 @@ KEYWORD_LIST = [
     # '雅诗兰黛~(estee lauder)',
     # 'Whoo',
     # '资生堂~shiseido',
-    '兰蔻~lancome'
+    '兰蔻'
     # '赫莲娜~(helena rubinstein)',
     # '海蓝之谜~lamer~(la mer)',
     # 'skii~sk-ii',
