b84c0630bde73b611fa05c96ed261425e83b2504
schen6 <sailchen@gmail.com>
2021-05-20 00:29:39 +0800

    token

--- exec.sh
+++ exec.sh
@@ -2,5 +2,5 @@
 cd /home/panther
 . ./venv/bin/activate
 cd weibo-search
-scrapy crawl search
-#SCRAPY_PROJECT=GrpThree scrapy crawl search
+#scrapy crawl search
+SCRAPY_PROJECT=GrpOne scrapy crawl search
