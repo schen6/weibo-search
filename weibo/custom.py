eb2a5b9f4e45b2f50a2eb1953e7889a063ce5628
schen6 <sailchen@gmail.com>
2021-07-02 17:25:21 +0800

    token

--- weibo/custom.py
+++ weibo/custom.py
@@ -30,7 +30,7 @@ yesterday = yesterday.strftime("%Y-%m-%d")
 
 client = p.PostgresWriter()
 sql = '''
-    select cookie from social.weibo_c where comp = 'mac'
+    select cookie from social.weibo_c where comp = 'lenovo'
 '''
 cookie = client.fetch_data(sql)
 cookie = cookie['cookie'][0]
@@ -68,7 +68,7 @@ ITEM_PIPELINES = {
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
 KEYWORD_LIST = [
-    '家具~家装'
+    '早饭~面包~吐司'
 ]  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
 WEIBO_TYPE = 1
@@ -76,11 +76,11 @@ WEIBO_TYPE = 1
 CONTAIN_TYPE = 0
 # 筛选微博的发布地区，精确到省或直辖市，值不应包含“省”或“市”等字，如想筛选北京市的微博请用“北京”而不是“北京市”，想要筛选安徽省的微博请用“安徽”而不是“安徽省”，可以写多个地区，
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用”全部“
-REGION = ['全部']
+REGION = ['上海']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = '2020-09-11'  # '2021-04-17'
+START_DATE = '2021-01-01'  # '2021-04-17'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = '2021-06-23'
+END_DATE = '2021-06-30'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
 FURTHER_THRESHOLD = 46
