45256f20d6990e7e3c5c0d1a896f99c7166abd9a
schen6 <sailchen@gmail.com>
2021-07-08 18:20:11 +0800

    token

--- weibo/kiehls.py
+++ weibo/kiehls.py
@@ -5,6 +5,7 @@ import sys
 sys.path.append('/home/panther/')
 import vagabond.crawlers.PostgresWriter as p
 import os
+import vagabond.tools.scrapydt as scrapydt
 
 import configparser
 cfp = configparser.ConfigParser()
@@ -12,16 +13,7 @@ root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 cfp_path = root_path + '/social.conf'
 cfp.read(cfp_path)
 
-today = date.today()
-yesterday = today + datetime.timedelta(days=-1)
-today = today.strftime("%Y-%m-%d")
-yesterday = yesterday.strftime("%Y-%m-%d")
-
-if datetime.datetime.now().hour <= 17:
-    dt = yesterday
-else:
-    dt = today
-
+st_dt, end_dt = scrapydt.get_dates()
 if cfp.get('date','crawl_date'):
     dt = cfp.get('date','crawl_date')
     # t = datetime.datetime.strptime(dt, '%Y-%m-%d')
@@ -111,9 +103,9 @@ CONTAIN_TYPE = 0
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用”全部“
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = dt  # '2021-04-17'
+START_DATE = st_dt  # '2021-04-17'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = dt  # '2021-04-18'
+END_DATE = end_dt  # '2021-04-18'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
 FURTHER_THRESHOLD = 46
