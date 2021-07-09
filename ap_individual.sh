04277ef6846794478a63a4517b77daebb8add146
schen6 <sailchen@gmail.com>
2021-07-09 10:21:53 +0800

    token

--- ap_individual.sh
+++ ap_individual.sh
@@ -5,3 +5,33 @@ cd weibo-search
 #scrapy crawl search
 SCRAPY_PROJECT=chando scrapy crawl search
 SCRAPY_PROJECT=clarins scrapy crawl search
+SCRAPY_PROJECT=clinique scrapy crawl search
+SCRAPY_PROJECT=dryu scrapy crawl search
+SCRAPY_PROJECT=elixir scrapy crawl search
+SCRAPY_PROJECT=estee scrapy crawl search
+SCRAPY_PROJECT=florasis scrapy crawl search
+SCRAPY_PROJECT=freeplus scrapy crawl search
+SCRAPY_PROJECT=herborist scrapy crawl search
+SCRAPY_PROJECT=hr scrapy crawl search
+SCRAPY_PROJECT=innisfree scrapy crawl search
+SCRAPY_PROJECT=ipsa scrapy crawl search
+SCRAPY_PROJECT=kiehls scrapy crawl search
+SCRAPY_PROJECT=lamer scrapy crawl search
+SCRAPY_PROJECT=lancome scrapy crawl search
+SCRAPY_PROJECT=laneige scrapy crawl search
+SCRAPY_PROJECT=loccitane scrapy crawl search
+SCRAPY_PROJECT=loreal scrapy crawl search
+SCRAPY_PROJECT=mamonde scrapy crawl search
+SCRAPY_PROJECT=marubi scrapy crawl search
+SCRAPY_PROJECT=olay scrapy crawl search
+SCRAPY_PROJECT=origins scrapy crawl search
+SCRAPY_PROJECT=perfectdiary scrapy crawl search
+SCRAPY_PROJECT=proya scrapy crawl search
+SCRAPY_PROJECT=shiseido scrapy crawl search
+SCRAPY_PROJECT=skii scrapy crawl search
+SCRAPY_PROJECT=skinceuticals scrapy crawl search
+SCRAPY_PROJECT=sulwhasoo scrapy crawl search
+SCRAPY_PROJECT=whoo scrapy crawl search
+SCRAPY_PROJECT=winona scrapy crawl search
+
+
