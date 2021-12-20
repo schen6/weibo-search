10e0bdc020f11bf3224db87b78e60536c68f9654
schen6 <sailchen@gmail.com>
2021-12-20 14:56:53 +0800

    close pg connections

--- weibo/spiders/search.py
+++ weibo/spiders/search.py
@@ -70,7 +70,7 @@ class SearchSpider(scrapy.Spider):
     pymysql_error = False
 
     def start_requests(self):
-        proxy = get_proxy3('H1D2G2X93S0Z0K4D', 'D2CA54BD12709B90')
+        proxy = get_proxy3('H13T90QR94T8X68D', 'F4BCD137F4869682')
         proxies = 'http://' + proxy
         start_date = datetime.strptime(self.start_date, '%Y-%m-%d')
         end_date = datetime.strptime(self.end_date,
@@ -126,7 +126,7 @@ class SearchSpider(scrapy.Spider):
             raise CloseSpider()
 
     def parse(self, response):
-        proxy = get_proxy3('H1D2G2X93S0Z0K4D', 'D2CA54BD12709B90')
+        proxy = get_proxy3('H13T90QR94T8X68D', 'F4BCD137F4869682')
         proxies = 'http://' + proxy
         base_url = response.meta.get('base_url')
         keyword = response.meta.get('keyword')
@@ -173,7 +173,7 @@ class SearchSpider(scrapy.Spider):
 
     def parse_by_day(self, response):
         """以天为单位筛选"""
-        proxy = get_proxy3('H1D2G2X93S0Z0K4D', 'D2CA54BD12709B90')
+        proxy = get_proxy3('H13T90QR94T8X68D', 'F4BCD137F4869682')
         proxies = 'http://' + proxy
         base_url = response.meta.get('base_url')
         keyword = response.meta.get('keyword')
@@ -223,7 +223,7 @@ class SearchSpider(scrapy.Spider):
                                      })
 
     def parse_by_hour(self, response):
-        proxy = get_proxy3('H1D2G2X93S0Z0K4D', 'D2CA54BD12709B90')
+        proxy = get_proxy3('H13T90QR94T8X68D', 'F4BCD137F4869682')
         proxies = 'http://' + proxy
 
         """以小时为单位筛选"""
@@ -286,7 +286,7 @@ class SearchSpider(scrapy.Spider):
 
     def parse_by_hour_province(self, response):
         """以小时和直辖市/省为单位筛选"""
-        proxy = get_proxy3('H1D2G2X93S0Z0K4D', 'D2CA54BD12709B90')
+        proxy = get_proxy3('H13T90QR94T8X68D', 'F4BCD137F4869682')
         proxies = 'http://' + proxy
         keyword = response.meta.get('keyword')
         is_empty = response.xpath(
@@ -331,7 +331,7 @@ class SearchSpider(scrapy.Spider):
 
     def parse_page(self, response):
         """解析一页搜索结果的信息"""
-        proxy = get_proxy3('H1D2G2X93S0Z0K4D', 'D2CA54BD12709B90')
+        proxy = get_proxy3('H13T90QR94T8X68D', 'F4BCD137F4869682')
         proxies = 'http://' + proxy
         keyword = response.meta.get('keyword')
         is_empty = response.xpath(
