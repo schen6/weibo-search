0df84974072a3af7d1af5c9e6532dcfb3dc5e174
schen6 <sailchen@gmail.com>
2022-04-13 12:30:27 +0800

    close pg connections

--- ryo_custom_run.sh
+++ ryo_custom_run.sh
@@ -7,11 +7,11 @@ cd weibo-search
 #SCRAPY_PROJECT=kerastase_h scrapy crawl search
 #SCRAPY_PROJECT=kono_h scrapy crawl search
 #SCRAPY_PROJECT=lorealhair_h scrapy crawl search
-SCRAPY_PROJECT=renefurterer_h scrapy crawl search
+#SCRAPY_PROJECT=renefurterer_h scrapy crawl search
 SCRAPY_PROJECT=ryo_h scrapy crawl search
 #SCRAPY_PROJECT=schwarzkopf_h scrapy crawl search
 #SCRAPY_PROJECT=seeyoung_h scrapy crawl search
-SCRAPY_PROJECT=triptych_h scrapy crawl search
+#SCRAPY_PROJECT=triptych_h scrapy crawl search
 
 #SCRAPY_PROJECT=bodyaid_h scrapy crawl search
 #SCRAPY_PROJECT=hairrecipe_h scrapy crawl search
--- weibo/debeers/debeers.py
+++ weibo/debeers/debeers.py
@@ -16,21 +16,21 @@ cfp.read(cfp_path)
 # st_dt, end_dt = scrapydt.get_dates()
 
 if cfp.get('date','crawl_date'):
-    st_dt = cfp.get('date','crawl_date')
-    end_dt = st_dt
-    # t = datetime.datetime.strptime(dt, '%Y-%m-%d')
-    # if t.date() < datetime.datetime.now().date():
-    #     t = t + datetime.timedelta(days=1)
-    #     new_t = t.strftime("%Y-%m-%d")
-    # cfp.set('date', 'crawl_date', '')
-    # with open(cfp_path, 'w') as configfile:
-    #     cfp.write(configfile)
+    dt = cfp.get('date','crawl_date')
+    t = datetime.datetime.strptime(dt, '%Y-%m-%d')
+    if t.date() < datetime.datetime.now().date():
+        t = t + datetime.timedelta(days=1)
+        new_t = t.strftime("%Y-%m-%d")
+    cfp.set('date', 'crawl_date', new_t)
+    with open(cfp_path, 'w') as configfile:
+        cfp.write(configfile)
 
 client = p.PostgresWriter()
 sql = '''
     select cookie from social.weibo_c where comp = 'lenovo'
 '''
 cookie = client.fetch_data(sql)
+client.client.close()
 cookie = cookie['cookie'][0]
 
 BOT_NAME = 'weibo'
@@ -66,49 +66,23 @@ ITEM_PIPELINES = {
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
 KEYWORD_LIST = [
-    # '雪花秀~sulwhasoo',
-    # '雅诗兰黛~(estee lauder)',
-    # 'Whoo',
-    # '资生堂~shiseido',
-    # '兰蔻~lancome'
-    # '赫莲娜~(helena rubinstein)',
-    # '海蓝之谜~lamer~(la mer)',
-    # 'skii~sk-ii',
-    # '修丽可~skinceuticals',
-    # '兰芝~laneige', #1
-    # '倩碧~clinique',
-    # '科颜氏~kiehl~kiehls~kiehl\'s',
-    # '欧莱雅~loreal',
-    # '怡丽丝尔~elixir',
-    # '茵芙莎~ipsa',
-    # '娇韵诗~clarins',
-    # '悦木之源~origins -金钟炫',
-    # '欧舒丹~l\'occitane~loccitane',
-    # '悦诗风吟~innisfree',
-    # '梦妆~mamonde'
-    # '玉兰油~olay',
-    # '自然堂~chando',
-    # '芙丽芳丝~freeplus',
-    # '完美日记~(perfect diary)',
-    # '花西子~florasis',
-    # '玉泽~(dr yu)',
-    # '丸美~marubi',
-    # '佰草集~herborist',
-    #'吕ryo~吕洗发水~ryo吕~红吕~白吕~粉吕~紫吕~金吕~绿吕~@吕Ryo'
-    '@吕Ryo'
-    # '珀莱雅~proya'
+    '(de beers)~debeers~戴比尔斯',
+    '蒂芙妮～tiffany',
+    'cartier~卡地亚',
+    'bvlgari～宝格丽',
+    'vca~(van cleef)~梵克雅宝'
 ]  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
-WEIBO_TYPE = 0
+WEIBO_TYPE = 1
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
 CONTAIN_TYPE = 0
 # 筛选微博的发布地区，精确到省或直辖市，值不应包含“省”或“市”等字，如想筛选北京市的微博请用“北京”而不是“北京市”，想要筛选安徽省的微博请用“安徽”而不是“安徽省”，可以写多个地区，
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用”全部“
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = st_dt  # '2021-04-17'
+START_DATE = dt  # '2021-04-17'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = end_dt  # '2021-04-18'
+END_DATE = dt  # '2021-04-18'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
 FURTHER_THRESHOLD = 46
