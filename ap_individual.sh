ccbe13799dd5506e427565fa3a4e7a1fbb56ff93
schen6 <sailchen@gmail.com>
2022-08-05 20:54:35 +0800

    add new brands

--- ap_individual.sh
+++ ap_individual.sh
@@ -4,12 +4,12 @@ cd /home/panther
 cd weibo-search
 #scrapy crawl search
 SCRAPY_PROJECT=chando scrapy crawl search
-SCRAPY_PROJECT=clarins scrapy crawl search
+#SCRAPY_PROJECT=clarins scrapy crawl search
 SCRAPY_PROJECT=clinique scrapy crawl search
 SCRAPY_PROJECT=dryu scrapy crawl search
 SCRAPY_PROJECT=elixir scrapy crawl search
 SCRAPY_PROJECT=estee scrapy crawl search
-SCRAPY_PROJECT=florasis scrapy crawl search
+#SCRAPY_PROJECT=florasis scrapy crawl search
 SCRAPY_PROJECT=freeplus scrapy crawl search
 SCRAPY_PROJECT=herborist scrapy crawl search
 SCRAPY_PROJECT=hr scrapy crawl search
@@ -19,13 +19,13 @@ SCRAPY_PROJECT=kiehls scrapy crawl search
 SCRAPY_PROJECT=lamer scrapy crawl search
 SCRAPY_PROJECT=lancome scrapy crawl search
 SCRAPY_PROJECT=laneige scrapy crawl search
-SCRAPY_PROJECT=loccitane scrapy crawl search
+#SCRAPY_PROJECT=loccitane scrapy crawl search
 SCRAPY_PROJECT=loreal scrapy crawl search
 SCRAPY_PROJECT=mamonde scrapy crawl search
 SCRAPY_PROJECT=marubi scrapy crawl search
 SCRAPY_PROJECT=olay scrapy crawl search
 SCRAPY_PROJECT=origins scrapy crawl search
-SCRAPY_PROJECT=perfectdiary scrapy crawl search
+#SCRAPY_PROJECT=perfectdiary scrapy crawl search
 SCRAPY_PROJECT=proya scrapy crawl search
 SCRAPY_PROJECT=shiseido scrapy crawl search
 SCRAPY_PROJECT=skii scrapy crawl search
@@ -34,15 +34,15 @@ SCRAPY_PROJECT=sulwhasoo scrapy crawl search
 SCRAPY_PROJECT=whoo scrapy crawl search
 SCRAPY_PROJECT=winona scrapy crawl search
 
-SCRAPY_PROJECT=adolph scrapy crawl search
+#SCRAPY_PROJECT=adolph scrapy crawl search
 SCRAPY_PROJECT=kerastase scrapy crawl search
 SCRAPY_PROJECT=kono scrapy crawl search
 SCRAPY_PROJECT=lorealhair scrapy crawl search
 SCRAPY_PROJECT=renefurterer scrapy crawl search
 SCRAPY_PROJECT=ryo scrapy crawl search
 SCRAPY_PROJECT=schwarzkopf scrapy crawl search
-SCRAPY_PROJECT=seeyoung scrapy crawl search
-SCRAPY_PROJECT=triptych scrapy crawl search
+#SCRAPY_PROJECT=seeyoung scrapy crawl search
+#SCRAPY_PROJECT=triptych scrapy crawl search
 SCRAPY_PROJECT=bodyaid scrapy crawl search
 SCRAPY_PROJECT=hairrecipe scrapy crawl search
 
@@ -52,5 +52,8 @@ SCRAPY_PROJECT=sisley scrapy crawl search
 SCRAPY_PROJECT=valmont scrapy crawl search
 SCRAPY_PROJECT=guerlain scrapy crawl search
 SCRAPY_PROJECT=cpb scrapy crawl search
+SCRAPY_PROJECT=sassoon scrapy crawl search
+SCRAPY_PROJECT=spes scrapy crawl search
+SCRAPY_PROJECT=kans scrapy crawl search
 
 
